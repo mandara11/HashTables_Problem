1c62ef97024e5287be46b91db709cc4709d2f3c1
mandara11 <mandaram2345@gmail.com>
2022-08-16 07:07:56 +0530

    Added:UC2

--- Program.cs
+++ Program.cs
@@ -5,33 +5,36 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to HashTable problem");
-            HashMap<string, int> hash = new HashMap<string, int>(5);
-            string words = "to be or not to be";
-            string[] arr = words.Split(' ');
-            LinkedList<string> checkForDuplication = new LinkedList<string>();
-            foreach (string element in arr) // to -> be
+            MyMapNode<string, int> hash = new MyMapNode<string, int>(5);
+            //UC2
+            string phrase = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            string[] arr1 = phrase.Split(' ');
+            LinkedList<string> checkDuplication = new LinkedList<string>();
+            foreach (string element in arr1)
             {
                 int count = 0;
-                foreach (string match in arr) //to->be->or->not->to-> be-> 
+                foreach (string match in arr1)
                 {
                     if (element == match)
                     {
-                        count++;//1->2
-                        if (checkForDuplication.Contains(element))
+                        count++;
+                        if (checkDuplication.Contains(element))
                         {
                             break;
                         }
                     }
 
                 }
-                if (!checkForDuplication.Contains(element))
-                {
-                    checkForDuplication.AddLast(element);
-                    hash.Add(element, count);//(to,2)
 
+                if (checkDuplication.Contains(element))
+                {
+                    continue;
                 }
+                checkDuplication.AddLast(element);
+                hash.Add(element, count);
             }
             hash.Display();
+
         }
     }
 }
