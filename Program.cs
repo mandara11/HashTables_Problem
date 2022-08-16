275557a6e7cb9985766da1e2fbc525e08413ca51
mandara11 <mandaram2345@gmail.com>
2022-08-16 07:17:07 +0530

    Added:UC3

--- Program.cs
+++ Program.cs
@@ -5,35 +5,37 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to HashTable problem");
-            MyMapNode<string, int> hash = new MyMapNode<string, int>(5);
-            //UC2
-            string phrase = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
-            string[] arr1 = phrase.Split(' ');
-            LinkedList<string> checkDuplication = new LinkedList<string>();
-            foreach (string element in arr1)
+            MyMapNode<string, int> hash2= new MyMapNode<string, int>(5);
+            string phrase2 = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            string[] arr2 = phrase2.Split(' ');
+            LinkedList<string> checkDuplicate = new LinkedList<string>();
+            foreach (string element in arr2)
             {
                 int count = 0;
-                foreach (string match in arr1)
+                foreach (string match in arr2)
                 {
                     if (element == match)
                     {
                         count++;
-                        if (checkDuplication.Contains(element))
+                        if (checkDuplicate.Contains(element))
                         {
                             break;
                         }
                     }
-
                 }
-
-                if (checkDuplication.Contains(element))
+                if (checkDuplicate.Contains(element))
                 {
                     continue;
                 }
-                checkDuplication.AddLast(element);
-                hash.Add(element, count);
+                checkDuplicate.AddLast(element);
+                hash2.Add(element, count);
             }
-            hash.Display();
+            int freq = hash2.Get("avoidable");
+            Console.WriteLine("Frequency of the word Avoidable:" + "" + freq);
+            hash2.Remove("avoidable");
+            freq = hash2.Get("avoidable");
+            Console.WriteLine("Frequency of the word Avoidable after removing:" + "" + freq);
+            hash2.Display();
 
         }
     }
